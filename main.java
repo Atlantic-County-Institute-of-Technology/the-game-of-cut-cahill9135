2bb11aa6ee52e4b86f9e6cd3772e68b6e6741939
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2025-10-28 12:55:55 -0400

    Create main.java
    main

---
+++ main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int cpuWins = 0;
+        int plyrWins = 0;
+
+        while(cpuWins < 3 && plyrWins < 3){
+            cpuWins++;
+        }
+
+
+
+    }
+}
