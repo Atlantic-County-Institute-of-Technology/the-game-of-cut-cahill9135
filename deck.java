dd6a72f8d06b9821d293a2245e7fe9511b3c6dd0
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2025-10-28 12:57:56 -0400

    Create deck.java
    deck

---
+++ deck.java
@@ -0,0 +1,14 @@
+public class deck {
+private card[] deck = {null};
+
+
+    public deck() {
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 13; j++){
+                deck[] new card(i, j);
+            }
+        }
+
+    }
+}
