e2bd60b857f9f9f59c29df598d000ca31e7a07dc
cahill9135 <126170328+cahill9135@users.noreply.github.com>
2025-11-03 12:56:21 -0500

    Update deck.java
    More progress i suppose

--- deck.java
+++ deck.java
@@ -1,14 +1,37 @@
-public class deck {
-private card[] deck = {null};
+import java.util.Random;
+
+public class arrDeck {
 
 
-    public deck() {
+    private card[] deck;
+    private final Random shuffler = new Random();
+    private final int DECK_SIZE = 52;
 
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 13; j++){
-                deck[] new card(i, j);
+    public arrDeck(){
+        deck = new card[DECK_SIZE];
+    }
+
+    public void populateDeck() {
+        int index = 0;
+        for (int suit = 1; suit <= 4; suit++) {
+            for (int face = 1; face <= 13; face++) {
+                deck[index] = new card(face, suit);
+                index++;
             }
         }
-
     }
+
+    public card selectCard(int target) {
+        return deck[target];
+    }
+
+    public String showDeck() {
+        String result = "";
+
+        for(int i = 0; i < DECK_SIZE; i++) {
+            result += deck[i].toString() + "\n";
+        }
+        return result;
+    }
+
 }
